9b9333579dce829305cdd36b5439dd162c399e22
Artur Iury <51382714+Artursalviano@users.noreply.github.com>
2026-05-15 23:20:25 -0300

    testes de pacotos da dbt/utils

---
+++ models/customers_daily_summary.sql
@@ -0,0 +1,9 @@
+select
+customer_id,
+order_date,
+{{ dbt_utils.generate_surrogate_key(['customer_id', 'order_date']) }} as pk,
+count(*) as total_orders
+
+from {{ref('stg_jaffle_shop__orders')}}
+
+group by all
